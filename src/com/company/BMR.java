ea571dfa1ed1f66249784738a1ef17ae8cf112b0
Niels <nbijl@petersplanet.nl>
2020-06-08 10:47:46 +0200

    BMR formulas

--- src/com/company/BMR.java
+++ src/com/company/BMR.java
@@ -2,4 +2,40 @@ package com.company;
 
 public class BMR {
 
+    public static double harrisBenedict (Body body){
+        double productionOfHeatAtCompleteRest;
+        if (body.gender == "male"){
+            productionOfHeatAtCompleteRest = (13.7516 * body.getMass()) / 1 + (5.0033 * body.getHeight()) / 1 - (6.7550 * body.getAge()) / 1 + 66.4730;
+        }
+        else {
+            productionOfHeatAtCompleteRest = (9.5634 * body.getMass()) / 1 + (1.8496 * body.getHeight()) / 1 - (4.6756 * body.getAge()) / 1 + 655.0955;
+        }
+        return productionOfHeatAtCompleteRest;
+    }
+
+    public static double harrisBenedictRevised(Body body){
+        double productionOfHeatAtCompleteRest;
+        if (body.gender == "male"){
+            productionOfHeatAtCompleteRest = (13.397 * body.getMass()) / 1 + (4.799 * body.getHeight()) / 1 - (5.677 * body.getAge()) / 1 + 88.362;
+        }
+        else {
+            productionOfHeatAtCompleteRest = (9.247 * body.getMass()) / 1 + (3.098 * body.getHeight()) / 1 - (4.330 * body.getAge()) / 1 + 447.593;
+        }
+        return productionOfHeatAtCompleteRest;
+    }
+    public static double mifflinStJeor(Body body){
+        double productionOfHeatAtCompleteRest;
+        int s = -161;
+        if (body.gender == "male"){
+            s = 5;
+        }
+        productionOfHeatAtCompleteRest = (10.0 * body.getMass()) / 1 + (6.25 * body.getHeight()) / 1 - (5.0 * body.getAge()) / 1 + s;
+        return productionOfHeatAtCompleteRest;
+    }
+    public static double katchMcArdle(Body body){
+        double productionOfHeatAtCompleteRest;
+        double leanBodyMass = body.getMass() * (1- body.getBodyFatPercentage()/100);
+        productionOfHeatAtCompleteRest = 370 + 21.6 * leanBodyMass;
+        return productionOfHeatAtCompleteRest;
+    }
 }
--- src/com/company/Body.java
+++ src/com/company/Body.java
@@ -2,25 +2,36 @@ package com.company;
 
 public class Body {
     private String name;
-    private double productionOfHeat;
+    private double productionOfHeatAtCompleteRest;
     private double mass;
     private double height;
     private int age;
+    final String gender;
+    private int bodyFatPercentage;
 
     //constructor
-    public Body(String name, double massKG, double heightCM, int ageYEARS){
+    public Body(String name, double massKG, double heightCM, int ageYEARS, String gender){
         this.name = name;
         this.mass = massKG;
         this.height = heightCM;
         this.age = ageYEARS;
+        this.gender = gender;
+    }
+    public Body(String name, double massKG, double heightCM, int ageYEARS, String gender, int bodyFatPercentage){
+        this.name = name;
+        this.mass = massKG;
+        this.height = heightCM;
+        this.age = ageYEARS;
+        this.gender = gender;
+        this.bodyFatPercentage = bodyFatPercentage;
     }
 
     //setters
     void setName(String name){
         this.name = name;
     }
-    void setProductionOfHeat(double productionOfHeat){
-        this.productionOfHeat = productionOfHeat;
+    void setProductionOfHeatAtCompleteRest(double productionOfHeatAtCompleteRest){
+        this.productionOfHeatAtCompleteRest = productionOfHeatAtCompleteRest;
     }
     void setMass(double massKG){
         this.mass = massKG;
@@ -31,13 +42,16 @@ public class Body {
     void setAge(int ageYEARS){
         this.age = ageYEARS;
     }
+    void setBodyFatPercentage(int bodyFatPercentage){
+        this.bodyFatPercentage = bodyFatPercentage;
+    }
 
     //getters
     String getName(){
         return this.name;
     }
-    double getProductionOfHeat(){
-        return this.productionOfHeat;
+    double getProductionOfHeatAtCompleteRest(){
+        return this.productionOfHeatAtCompleteRest;
     }
     double getMass(){
         return this.mass;
@@ -47,6 +61,12 @@ public class Body {
     }
     int getAge(){
         return this.age;
+    }
+    String getGender(){
+        return this.gender;
+    }
+    int getBodyFatPercentage(){
+        return this.bodyFatPercentage;
     }
 
 
